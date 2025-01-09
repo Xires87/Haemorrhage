b2ebc9c53c065b3ea08e0533a7fa7b5d960bf4ef
Xires87 <frycmod@gmail.com>
2025-01-09 11:41:43 +0100

    it doesnt change anything but why not

--- src/main/java/net/fryc/imbleeding/network/s2c/CreateBloodParticleS2CPacket.java
+++ src/main/java/net/fryc/imbleeding/network/s2c/CreateBloodParticleS2CPacket.java
@@ -6,8 +6,8 @@ import net.fryc.imbleeding.effects.particles.ModParticles;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.world.World;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -16,9 +16,9 @@ public class CreateBloodParticleS2CPacket {
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender){
         ClientPlayerEntity player = client.player;
         if(player != null){
-            World world = player.getWorld();
-            if(world.isClient()){
+            if(player.getWorld().isClient()){
                 if(ImBleeding.config.enableBloodParticlesClientSided){
+                    ClientWorld world = (ClientWorld) player.getWorld();
                     double x = buf.readDouble() + (ThreadLocalRandom.current().nextFloat()/2) - 0.25f;
                     double y = buf.readDouble() + ThreadLocalRandom.current().nextFloat() + 0.1f;
                     double z = buf.readDouble() + (ThreadLocalRandom.current().nextFloat()/2) - 0.25f;
