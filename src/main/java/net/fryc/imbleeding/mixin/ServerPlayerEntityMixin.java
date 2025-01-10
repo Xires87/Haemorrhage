17a0c04123024ecfd012c700e1ba658403a2fe3a
Xires87 <frycmod@gmail.com>
2025-01-10 12:21:16 +0100

    particle randomisation on client side

--- src/main/java/net/fryc/imbleeding/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/fryc/imbleeding/mixin/ServerPlayerEntityMixin.java
@@ -7,6 +7,7 @@ import net.fryc.imbleeding.effects.ModEffects;
 import net.fryc.imbleeding.network.payloads.CreateBloodParticlePayload;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -15,12 +16,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Mixin(ServerPlayerEntity.class)
 abstract class ServerPlayerEntityMixin extends PlayerEntity {
-
-    private Random myRandom = new Random();
 
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
@@ -28,15 +27,14 @@ abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @Inject(method = "tick()V", at = @At("TAIL"))
     private void spawnBloodParticles(CallbackInfo info) {
-        ServerPlayerEntity dys = ((ServerPlayerEntity)(Object)this);
-        if(dys.hasStatusEffect(ModEffects.BLEED_EFFECT)){
-            if(this.myRandom.nextInt(100) < 5 + dys.getActiveStatusEffects().get(ModEffects.BLEED_EFFECT).getAmplifier()*5){
-                Vec3d vec3d = dys.getVelocity();
-                for (ServerPlayerEntity pl : PlayerLookup.tracking(dys.getServerWorld(), dys.getChunkPos())) {
+        if(this.hasStatusEffect(ModEffects.BLEED_EFFECT)){
+            if(ThreadLocalRandom.current().nextInt(100) < 5 + this.getActiveStatusEffects().get(ModEffects.BLEED_EFFECT).getAmplifier()*5){
+                Vec3d vec3d = this.getVelocity();
+                for (ServerPlayerEntity pl : PlayerLookup.tracking(((ServerWorld) this.getWorld()), this.getChunkPos())) {
                     ServerPlayNetworking.send(pl, new CreateBloodParticlePayload(
-                            dys.getX() + (this.myRandom.nextFloat()/2) - 0.25f,
-                            dys.getY() + this.myRandom.nextFloat() + 0.1f,
-                            dys.getZ() + (this.myRandom.nextFloat()/2) - 0.25f,
+                            this.getX(),
+                            this.getY(),
+                            this.getZ(),
                             vec3d.getY()-0.05
                     ));
                 }
--- src/main/java/net/fryc/imbleeding/network/s2c/CreateBloodParticleS2CPacket.java
+++ src/main/java/net/fryc/imbleeding/network/s2c/CreateBloodParticleS2CPacket.java
@@ -5,21 +5,26 @@ import net.fryc.imbleeding.ImBleeding;
 import net.fryc.imbleeding.effects.particles.ModParticles;
 import net.fryc.imbleeding.network.payloads.CreateBloodParticlePayload;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.world.World;
+import net.minecraft.client.world.ClientWorld;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CreateBloodParticleS2CPacket {
 
     public static void receive(CreateBloodParticlePayload payload, ClientPlayNetworking.Context context){
         ClientPlayerEntity player = context.player();
         if(player != null){
-            if(ImBleeding.config.enableBloodParticlesClientSided){
-                World world = player.getWorld();
-                double x = payload.x();
-                double y = payload.y();
-                double z = payload.z();
-                double vec3d = payload.vec3d();
-                world.addParticle(ModParticles.BLOOD_PARTICLE, x, y, z, 0, vec3d, 0);
+            if(player.getWorld().isClient()){
+                if(ImBleeding.config.enableBloodParticlesClientSided){
+                    ClientWorld world = ((ClientWorld) player.getWorld());
+                    double x = payload.x() + (ThreadLocalRandom.current().nextFloat()/2) - 0.25f;
+                    double y = payload.y() + ThreadLocalRandom.current().nextFloat() + 0.1f;
+                    double z = payload.z() + (ThreadLocalRandom.current().nextFloat()/2) - 0.25f;
+                    double vec3d = payload.vec3d();
+                    world.addParticle(ModParticles.BLOOD_PARTICLE, x, y, z, 0, vec3d, 0);
+                }
             }
+
         }
     }
 }
