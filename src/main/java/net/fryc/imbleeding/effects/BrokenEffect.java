cf4dee80a9b4635a346f771862fb13ba23661aae
Xires87 <frycmod@gmail.com>
2025-08-14 12:36:03 +0200

    slowness is always applied when broken expires or is removed

--- src/main/java/net/fryc/imbleeding/effects/BrokenEffect.java
+++ src/main/java/net/fryc/imbleeding/effects/BrokenEffect.java
@@ -4,10 +4,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class BrokenEffect extends StatusEffect {
     protected BrokenEffect(StatusEffectCategory category, int color) {
         super(category, color);
+    }
+
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        super.onRemoved(entity, attributes, amplifier);
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 600, 0, false, false, true));
     }
 
     public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
