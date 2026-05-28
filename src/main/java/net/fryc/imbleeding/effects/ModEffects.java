d48455e312b55d975c46a047e32e29c4a5f4b8d8
Xires87 <lukasz005719@gmail.com>
2026-05-28 08:36:53 +0200

    removed unnecessary static fields

--- src/main/java/net/fryc/imbleeding/effects/ModEffects.java
+++ src/main/java/net/fryc/imbleeding/effects/ModEffects.java
@@ -19,11 +19,6 @@ public class ModEffects {
     public static RegistryEntry<StatusEffect> BROKEN;
     public static RegistryEntry<StatusEffect> WOUND_INFECTION;
 
-    private static final StatusEffect bleedout = new BleedoutEffect(StatusEffectCategory.HARMFUL, 16262179).addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH, Identifier.of(ImBleeding.MOD_ID, "effect.bleedout"), -12.0, EntityAttributeModifier.Operation.ADD_VALUE);
-    private static final StatusEffect bleed = new BleedEffect(StatusEffectCategory.HARMFUL, 16262179);
-    private static final StatusEffect healthloss = new HealthLossEffect(StatusEffectCategory.HARMFUL, 16262179).addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH, Identifier.of(ImBleeding.MOD_ID, "effect.health_loss"), -2.0, EntityAttributeModifier.Operation.ADD_VALUE);
-    private static final StatusEffect broken = new BrokenEffect(StatusEffectCategory.HARMFUL, 9154528).addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, Identifier.of(ImBleeding.MOD_ID, "effect.broken"), -0.15, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
-    private static final StatusEffect wound_infection = new WoundInfectionEffect(StatusEffectCategory.HARMFUL, 7889189).addAttributeModifier(ModEntityAttributes.GENERIC_BLEEDING_PROTECTION, Identifier.of(ImBleeding.MOD_ID, "effect.wound_infection"), -60, EntityAttributeModifier.Operation.ADD_VALUE);
 
     private static RegistryEntry<StatusEffect> registerStatusEffect(String id, StatusEffect statusEffect) {
         return Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(ImBleeding.MOD_ID, id), statusEffect);
@@ -33,11 +28,11 @@ public class ModEffects {
 
     public static void registerEffects() {
         if(BLEED_EFFECT == null){
-            BLEED_EFFECT = registerStatusEffect("bleed", bleed);
-            BLEEDOUT = registerStatusEffect("bleedout" , bleedout);
-            HEALTH_LOSS = registerStatusEffect("health_loss", healthloss);
-            BROKEN = registerStatusEffect("broken", broken);
-            WOUND_INFECTION = registerStatusEffect("wound_infection", wound_infection);
+            BLEED_EFFECT = registerStatusEffect("bleed", new BleedEffect(StatusEffectCategory.HARMFUL, 16262179));
+            BLEEDOUT = registerStatusEffect("bleedout" , new BleedoutEffect(StatusEffectCategory.HARMFUL, 16262179).addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH, Identifier.of(ImBleeding.MOD_ID, "effect.bleedout"), -12.0, EntityAttributeModifier.Operation.ADD_VALUE));
+            HEALTH_LOSS = registerStatusEffect("health_loss", new HealthLossEffect(StatusEffectCategory.HARMFUL, 16262179).addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH, Identifier.of(ImBleeding.MOD_ID, "effect.health_loss"), -2.0, EntityAttributeModifier.Operation.ADD_VALUE));
+            BROKEN = registerStatusEffect("broken", new BrokenEffect(StatusEffectCategory.HARMFUL, 9154528).addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, Identifier.of(ImBleeding.MOD_ID, "effect.broken"), -0.15, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+            WOUND_INFECTION = registerStatusEffect("wound_infection", new WoundInfectionEffect(StatusEffectCategory.HARMFUL, 7889189).addAttributeModifier(ModEntityAttributes.GENERIC_BLEEDING_PROTECTION, Identifier.of(ImBleeding.MOD_ID, "effect.wound_infection"), -60, EntityAttributeModifier.Operation.ADD_VALUE));
         }
     }
 }
